5d5ff341e111b8688f46a3c802b0020c2a5560ee
Maxi Giagnorio <maxigiagnorio@gmail.com>
2023-04-02 20:34:15 -0300

    modify access control from app.js

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,6 @@ require("./db.js");
 
 const server = express();
 
-
 server.name = "API";
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
@@ -16,7 +15,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
