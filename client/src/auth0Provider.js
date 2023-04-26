ac6b415dc78f90758b63cc56dd62ad3889b69b76
Maxi Giagnorio <maxigiagnorio@gmail.com>
2023-04-26 20:15:49 -0300

    fix login

--- client/src/auth0Provider.js
+++ client/src/auth0Provider.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
-import {Auth0Provider} from '@auth0/auth0-react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const Auth0ProviderWithHistory = ({children}) => {
-    const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-    const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const Auth0ProviderWithHistory = ({ children }) => {
+  // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const clientId =xq5SRNJEGjpib05vcCH6efuQFzuNxTkZ;
+  const domain =dev-i5wmro7l1pvr4q2b.us.auth0.com;
+  const history = useHistory();
 
-    const history = useHistory();
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
 
-    const onRedirectCallback = (appState) => {
-        history.push(appState?.returnTo || window.location.pathname);
-    };
-
-    return(
-        <Auth0Provider
-            domain={domain}
-            clientId={clientId}
-            redirectUri={window.location.origin}
-            onRedirectCallback={onRedirectCallback}
-        >
-           {children}
-        </Auth0Provider>
-    );
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export default Auth0ProviderWithHistory;
